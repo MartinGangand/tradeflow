45b1398f777452350d506f4963ee11b5f25a17be
Martin Gangand <martin.gangand1309@gmail.com>
2024-08-06 19:52:17 +0100

    Add setup.cfg

--- setup.py
+++ setup.py
@@ -1,49 +1,66 @@
+import os
+import sys
+
 from setuptools import Extension, setup, find_packages
-from setuptools.command.build_ext import build_ext as build_ext_orig
+from setuptools.command.build_ext import build_ext
 
 
 class CTypesExtension(Extension):
     pass
 
-# class build_ext(build_ext_orig):
+
+class new_build_ext(build_ext):
+
+    extra_compile_args = {
+        'tradeflow.simulate': {
+            'unix': ['-std=c++17'],
+            'msvc': ['/std:c++17']
+        }
+    }
+
+    def build_extension(self, ext):
+        extra_args = self.extra_compile_args.get(ext.name)
+        if extra_args is not None:
+            ctype = self.compiler.compiler_type
+            ext.extra_compile_args = extra_args.get(ctype, [])
+
+        build_ext.build_extension(self, ext)
+
     # def build_extension(self, ext):
     #     self._ctypes = isinstance(ext, CTypesExtension)
     #     return super().build_extension(ext)
-#
-#     def get_export_symbols(self, ext):
-#         if self._ctypes:
-#             return ext.export_symbols
-#         return super().get_export_symbols(ext)
-#
-#     def get_ext_filename(self, ext_name):
-#         if self._ctypes:
-#             return ext_name + ".so"
-#         return super().get_ext_filename(ext_name)
+    # def finalize_options(self):
+    #     super().finalize_options()
+    #     self.compiler = "unix"
 
-# class new_build_ext(build_ext_orig):
-#     def build_extension(self, ext):
-#         ext.extra_c_compile_args += std_c_flags(self)
-#         ext.extra_cxx_compile_args += std_cxx_flags(self)
-#         build_ext_orig.build_extension(self, ext)
+    def get_export_symbols(self, ext):
+        return ext.export_symbols
+
+    # def get_ext_filename(self, ext_name):
+    #     if self._ctypes:
+    #         return ext_name + ".so"
+    #     return super().get_ext_filename(ext_name)
 
 
 setup(
     packages=find_packages(),
-    install_requires=[
-           "matplotlib>=3.0.0",
-           "numpy>=1.21.0, < 2.0.0",
-           "pandas>=2.2.2",
-           "statsmodels>=0.12.2",
-           "pytest>=8.2.2"
-    ],
     py_modules=["tradeflow.ar_model"],
     ext_modules=[
         CTypesExtension(
             name="tradeflow.simulate",
             sources=["tradeflow/simulate.cpp"],
-            extra_compile_args=["-std=c++17"],
             language="c++"
         )
     ],
-    cmdclass={'build_ext': build_ext_orig}
+    cmdclass={'build_ext': new_build_ext}
 )
+
+
+# Windows:
+# COMPILER: <distutils._msvccompiler.MSVCCompiler> & msvc | OS: nt | SYS: win32
+#
+# Ubuntu:
+# COMPILER: <distutils.unixccompiler.UnixCCompiler> & unix | OS: posix | SYS: linux
+#
+# Mac:
+# COMPILER: <distutils.unixccompiler.UnixCCompiler> & unix | OS: posix | SYS: darwin
