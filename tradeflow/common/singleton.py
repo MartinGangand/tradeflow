1ce75379d8b53af5b6158f05876866b4b5e1bd6f
Martin Gangand <martin.gangand1309@gmail.com>
2024-12-06 08:22:31 +0000

    Thread safe singleton

--- tradeflow/common/singleton.py
+++ tradeflow/common/singleton.py
@@ -1,10 +1,13 @@
+from threading import Lock
+
+
 class Singleton(type):
 
     _instances = {}
+    _lock: Lock()
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(*args, **kwargs)
-        else:
-            raise RuntimeError("dd")
+        with cls._lock:
+            if cls not in cls._instances:
+                cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
