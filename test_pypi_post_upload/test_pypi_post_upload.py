0010f8969260cd80ac12c5068a5e48c5e2739487
Martin Gangand <martin.gangand1309@gmail.com>
2025-01-13 08:05:26 +0000

    Rename

--- test_pypi_post_upload/test_pypi_post_upload.py
+++ test_pypi_post_upload/test_pypi_post_upload.py
@@ -2,6 +2,7 @@ import importlib.metadata
 import subprocess
 import sys
 from pathlib import Path
+from typing import List
 
 import numpy as np
 import pytest
@@ -49,14 +50,19 @@ def test_end_to_end_from_package_installation_to_simulation_of_signs(index):
 
 def install_package(index: str) -> None:
     if index == "pypi":
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-cache-dir", PACKAGE_NAME])
+        subprocess.check_call(parse_command_line(f"{sys.executable}, -m pip install --no-cache-dir {PACKAGE_NAME}"))
     elif index == "test.pypi":
         # Install dependencies separately and then install the package without dependencies in order to install it from test.pypi.
         # Using 'pip install --index-url https://test.pypi.org/simple/ PACKAGE_NAME' does not work because it tries to install dependencies from test.pypi but some are not available.
         # Using 'pip install --index-url https://test.pypi.org/simple/ PACKAGE_NAME --extra-index-url https://pypi.org/simple/' does not work because if the package is also available on index pypi it will install it by default.
+
         requirements_file = ROOT_REPOSITORY.joinpath("requirements.txt")
         assert requirements_file.is_file()
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", str(requirements_file)])
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--index-url", f"https://test.pypi.org/simple/", "--no-deps", "--no-cache-dir", PACKAGE_NAME])
+        subprocess.check_call(parse_command_line(f"{sys.executable} -m pip install -r {str(requirements_file)}"))
+        subprocess.check_call(parse_command_line(f"{sys.executable} -m pip install --index-url https://test.pypi.org/simple/ --no-deps --no-cache-dir PACKAGE_NAME"))
     else:
         raise Exception(f"Unknown index {index}")
+
+
+def parse_command_line(command_line: str) -> List[str]:
+    return command_line.split()
