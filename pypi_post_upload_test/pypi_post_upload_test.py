76cf6fba74214bd966104e4491c51aec2c327061
Martin Gangand <martin.gangand1309@gmail.com>
2025-01-13 08:39:29 +0000

    Test upload

--- pypi_post_upload_test/pypi_post_upload_test.py
+++ pypi_post_upload_test/pypi_post_upload_test.py
@@ -22,44 +22,45 @@ def index(request):
     return request.config.getoption("--index")
 
 
-def test_end_to_end_from_package_installation_to_simulation_of_signs(index):
+def test_package_installation_and_simulation_of_signs(index):
     # Remove tradeflow from the module search path
     sys.path[:] = [path for path in sys.path if PACKAGE_NAME not in path]
 
-    # Check that the package is not already  installed or can't be accessed
+    # Check that the package is not already installed or can't be accessed
     with pytest.raises(ModuleNotFoundError) as ex:
         import tradeflow
     assert str(ex.value) == f"No module named '{PACKAGE_NAME}'"
 
-    # Install package and check that the version corresponds to version of the recently uploaded package
+    # Install the package and check that the installed version corresponds to the one from the freshly uploaded package
     install_package(index=index)
     installed_version = importlib.metadata.version(PACKAGE_NAME)
     assert installed_version == VERSION
 
     signs = np.loadtxt(DATA_FOLDER.joinpath("signs-20240720.txt"), dtype="int8", delimiter=",")
 
+    # Check package usage
     import tradeflow
     ar_model = tradeflow.AR(signs=signs, max_order=100, order_selection_method="pacf")
     ar_model = ar_model.fit(method="burg", significance_level=0.05, check_residuals=True)
-
     ar_model.simulate(size=1_000_000, seed=1)
     ar_model.simulation_summary(plot=True, log_scale=True)
 
-    subprocess.check_call([sys.executable, "-m", "pip", "uninstall", "-y", PACKAGE_NAME])
+    # Uninstall the package
+    subprocess.check_call(parse_command_line(f"{sys.executable} -m pip uninstall -y {PACKAGE_NAME}"))
 
 
 def install_package(index: str) -> None:
     if index == "pypi":
-        subprocess.check_call(parse_command_line(f"{sys.executable}, -m pip install --no-cache-dir {PACKAGE_NAME}"))
+        subprocess.check_call(parse_command_line(f"{sys.executable} -m pip install --no-cache-dir {PACKAGE_NAME}"))
     elif index == "test.pypi":
-        # Install dependencies separately and then install the package without dependencies in order to install it from test.pypi.
-        # Using 'pip install --index-url https://test.pypi.org/simple/ PACKAGE_NAME' does not work because it tries to install dependencies from test.pypi but some are not available.
-        # Using 'pip install --index-url https://test.pypi.org/simple/ PACKAGE_NAME --extra-index-url https://pypi.org/simple/' does not work because if the package is also available on index pypi it will install it by default.
+        # Install dependencies separately and then install the package from test.pypi without dependencies.
+        # 'pip install --index-url https://test.pypi.org/simple/ PACKAGE_NAME' does not work because it tries to install dependencies from index 'test.pypi' but some are not available.
+        # 'pip install --index-url https://test.pypi.org/simple/ PACKAGE_NAME --extra-index-url https://pypi.org/simple/' does not work because if the package is also available on index 'pypi', it will install it from there by default.
 
         requirements_file = ROOT_REPOSITORY.joinpath("requirements.txt")
         assert requirements_file.is_file()
         subprocess.check_call(parse_command_line(f"{sys.executable} -m pip install -r {str(requirements_file)}"))
-        subprocess.check_call(parse_command_line(f"{sys.executable} -m pip install --index-url https://test.pypi.org/simple/ --no-deps --no-cache-dir PACKAGE_NAME"))
+        subprocess.check_call(parse_command_line(f"{sys.executable} -m pip install --index-url https://test.pypi.org/simple/ --no-deps --no-cache-dir {PACKAGE_NAME}"))
     else:
         raise Exception(f"Unknown index {index}")
 
--- test_pypi_post_upload/conftest.py
+++
@@ -1,4 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption(
-        "--index", action="store", default="pypi", choices=("pypi", "test.pypi"), help="Specify the package index on which to validate the package. Use 'pypi' for the main Python Package Index or 'test.pypi' for the testing instance",
-    )
