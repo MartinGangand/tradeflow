7e4504100a1842ece5a8f9d3617a24f50cd8b160
Martin Gangand <93661497+MartinGangand@users.noreply.github.com>
2025-01-21 22:47:30 +0000

    Add job to automatically increment the project version after uploading to PyPi (#33)

--- pypi_post_upload_test/conftest.py
+++ pypi_post_upload_test/conftest.py
@@ -6,3 +6,8 @@ def pytest_addoption(parser):
         choices=("pypi", "test.pypi"),
         help="Specify the package index from which to install the package. Use 'pypi' for the main Python Package Index or 'test.pypi' for the testing instance."
     )
+    parser.addoption(
+        "--package_version",
+        action="store",
+        help="Specify the expected package version."
+    )
